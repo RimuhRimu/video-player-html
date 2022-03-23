a1e7adf85cbf20e5d83379cd89fd4041c9dc3dd5
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2022-03-22 23:19:45 -0400

    style(all): organizing better the code

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const $ = document
-const supportsVideo = !!$.createElement("video").canPlayType
+const supportsVideo = !!$.createElement("video").canPlayType 
+// check if support video media
 
 if (supportsVideo) {
   const video = $.getElementById("video")
-  video.controls = false
+  video.controls = false //ensuring that we have controls disable from the start
 
   const title = $.getElementById("title")
   const controls = $.getElementById("controls")
@@ -21,29 +22,77 @@ if (supportsVideo) {
 
   video.addEventListener("focus", hideUnhide)
   video.addEventListener("click", hideUnhide)
-
-  //show/hide loader when waiting for the video
+  
+  //show/hide loader when waiting for the video or  when is already loaded
   let loader = $.getElementById("loader")
   video.addEventListener("seeking", () => loader.style.display = "inline-block")
-  video.addEventListener("seeked", () => loader.style.display = "none")
   video.addEventListener("loadstart", () => loader.style.display = "inline-block")
   video.addEventListener("waiting", () => loader.style.display = "inline-block")
-
+  video.addEventListener("seeked", () => loader.style.display = "none")
+  video.addEventListener("loadeddata", () => loader.style.display = "none")
+  
   // Fontawesome classes
-  const [iconPlay, iconPause] = ["svg-inline--fa fa-pause playpause","svg-inline--fa fa-play playpause"]
+  const [iconPlay, iconPause] = ["svg-inline--fa fa-pause playpause icon","svg-inline--fa fa-play playpause icon"]
 
   //playpause logic
   const triggerPlayPause = () => {
     let playpause = $.getElementsByClassName("playpause")[0]
+    //changing the classes changes the icon
     if(video.paused || video.ended) {
       video.play()
-      playpause.className.baseVal = iconPlay
+      playpause.className.baseVal = iconPlay 
     }
     else {
       video.pause()
       playpause.className.baseVal = iconPause
     }
   }
+  
+  const playpauseContainer = $.getElementById("playpause")
+  //once data is loaded, it is safe to add the event listeners that needs that data
+  // and is safe to jump to in the video
+  video.addEventListener("loadeddata", () => {
+    playpauseContainer.addEventListener("click",() => {
+      triggerPlayPause()
+      hideUnhide()
+    })
+    $.addEventListener("keyup", (event) => {
+      let key = event.key.toLowerCase()
+      if(key === "enter" || key === " ") {
+        triggerPlayPause()
+        hideUnhide()
+      }
+    })
+    
+    //Jump to in the video
+    progress.addEventListener("click", (event) => {
+      let rect = progress.getBoundingClientRect()
+      let pos = (event.pageX - rect.left) / progress.offsetWidth
+      // this is explained in MDN https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery/cross_browser_video_player#skip_ahead
+      video.currentTime = pos * video.duration
+    })
+  })
+  
+  //update progress bar and duration
+  const progress = $.getElementById("progress")
+  video.addEventListener("loadedmetadata", () => {
+    const minutes = Math.floor(video.duration/60)
+    const seconds = Math.floor(video.duration)-minutes*60
+    duration.textContent = `${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`
+    progress.setAttribute("max", video.duration) //set max duration as value to the progress bar
+  })
+
+  video.addEventListener("timeupdate", () => {
+    loader.style.display = "none"
+    let playpause = playpauseContainer.children[0]
+    progress.value = video.currentTime
+    
+    const minutes = Math.floor(video.currentTime/60)
+    const seconds = Math.floor(video.currentTime)-minutes*60
+    currentDuration.textContent = `${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`
+    
+    if(video.ended) playpause.className = iconPause
+  })
 
   //raise and decrease volume logic
   const volSlider = $.getElementById("volSlider")
@@ -53,7 +102,7 @@ if (supportsVideo) {
 
   let volumeTrigger = () => {
     video.muted = !video.muted
-    const [volTrue,volFalse] = ["svg-inline--fa fa-volume-high","svg-inline--fa fa-volume-off"]
+    const [volTrue,volFalse] = ["svg-inline--fa fa-volume-high icon","svg-inline--fa fa-volume-off icon"]
     if(video.muted) {
       volume.children[0].className.baseVal = volFalse
     }
@@ -70,55 +119,7 @@ if (supportsVideo) {
     }
   })
   
-  //update progress bar and duration
-  const progress = $.getElementById("progress")
-  const playpauseContainer = $.getElementById("playpause")
-  const duration = $.getElementById("duration")
-  video.addEventListener("loadedmetadata", () => {
-    const minutes = Math.floor(video.duration/60)
-    const seconds = Math.floor(video.duration)-minutes*60
-    duration.textContent = `${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`
-    progress.setAttribute("max", video.duration)
-  })
-
-  video.addEventListener("timeupdate", () => {
-    loader.style.display = "none"
-    let playpause = playpauseContainer.children[0]
-    progress.value = video.currentTime
-    const minutes = Math.floor(video.currentTime/60)
-    const seconds = Math.floor(video.currentTime)-minutes*60
-    currentDuration.textContent = `${minutes > 9 ? minutes : `0${minutes}`}:${seconds > 9 ? seconds : `0${seconds}`}`
-    if(video.ended) playpause.className = iconPause
-  })
-  
-  //once data is loaded, it is safe to add the event listeners that needs that data
-  video.addEventListener("loadeddata", () => {
-
-    playpauseContainer.addEventListener("click",triggerPlayPause)
-    playpauseContainer.addEventListener("keyup", (event) => {
-      let key = event.key.toLowerCase()
-      if(key === "enter") {
-        triggerPlayPause()
-        hideUnhide()
-      }
-    })
-    $.addEventListener("keyup", (event) => {
-      let key = event.key.toLowerCase()
-      if(key === " ") {
-        triggerPlayPause()
-        hideUnhide()
-      }
-    })
-
-    //Jump to in the video
-    progress.addEventListener("click", (event) => {
-      let rect = progress.getBoundingClientRect()
-      let pos = (event.pageX - rect.left) / progress.offsetWidth
-      video.currentTime = pos * video.duration
-    })
-  })
-
-    //fullscreen logic, also check if fullscreen is supported
+   //fullscreen logic, also check if fullscreen is supported
   const container = $.querySelector("figure")
   const fullscreen = $.getElementById("fullscreen")
   const fullscreenEnabled = !!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitSupportsFullscreen || document.webkitFullscreenEnabled || document.createElement('video').webkitRequestFullScreen)
@@ -128,6 +129,7 @@ if (supportsVideo) {
   if (!fullscreenEnabled) fullscreen.style.display = 'none'
 
   fullscreen.addEventListener("click", () => handleFullscreen())
+  fullscreen.addEventListener("keyup", (e) => e.key === " " || e.key.toLowerCase() === "enter" ? handleFullscreen() : '')
 
   let handleFullscreen = () => {
     if (isFullscreen()) {
